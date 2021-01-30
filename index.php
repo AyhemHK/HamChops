0ba4c2cbc615d1f7f41b4610e162473d622de473
Ayhem Hanna Kachl <78269271+AyhemHK@users.noreply.github.com>
2021-01-30 11:04:36 -0500

    Initial Commit
    Pushing the main files which include the main stylesheet, icon, and html (in php extension).

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<title>HamChops</title>
+<link rel="stylesheet" href="css/main.css"></link>
+<link rel="icon" href="favicon.ico"></link>
+</head>
+
+<body>
+
+
+
+
+</body>
+
+</html>
