1f0aadd950f00bcb88854e40a31cff5e17fd1cf1
Ayhem Hanna Kachl <78269271+AyhemHK@users.noreply.github.com>
2021-01-30 12:28:02 -0500

    navbar
    finished rwd navbar

--- index.php
+++ index.php
@@ -1,15 +1,39 @@
 <!DOCTYPE html>
 <html lang="en">
 
+<!--head tag includes data of data (meta data)-->
 <head>
 <title>HamChops</title>
-<link rel="stylesheet" href="css/main.css"></link>
-<link rel="icon" href="favicon.ico"></link>
+<!--rel is the attribute of the type of file whereas href is the 
+destination of the file (ex .ico or .css)--> 
+	<link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet"><link rel="icon" href="favicon.ico"></link>
 </head>
 
-<body>
+<body class="bg-red-400">
+
+<nav class="font-sans flex flex-col text-center content-center sm:flex-row sm:text-left sm:justify-between py-2 px-6 bg-white shadow sm:items-baseline w-full">
+  <div class="mb-2 sm:mb-0 inner">
 
 
+    <div>
+	
+	<a href="/home" class="text-2xl no-underline text-grey-darkest hover:text-blue-dark font-sans font-bold ">HamChops</a>
+	
+	</div>
+	
+    <span class="text-xs text-grey-dark">We Got the Meats</span>
+
+  </div>
+
+  <div class="sm:mb-0 self-center">
+    <!-- <div class="h-10" style="display: table-cell, vertical-align: middle;"> -->
+    <a href="https://google.com" class="text-md no-underline text-black transition hover:text-blue-500 ml-2 px-1">Meats</a>
+    <a href="#" class="text-md no-underline text-grey-darker transition  hover:text-blue-500 ml-2 px-1">About Us</a>
+    <!-- <a href="/two" class="text-lg no-underline text-grey-darkest hover:text-blue-dark ml-2">About Us</a> -->
+    <a href="#" class="text-md no-underline text-grey-darker transition hover:text-blue-500 ml-2 px-1">Contact</a>
+    <!-- </div> -->
+  </div>
+</nav>
 
 
 </body>
