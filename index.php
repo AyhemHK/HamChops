662b16a58aafec97f651ec9ba99e3813334294e9
Daiyaan Ijaz <78269271+AyhemHK@users.noreply.github.com>
2021-02-05 18:51:31 -0500

    Updated Index Page
    We replaced our existing header and added a product section. Name and template is still ambiguous as I am just trying to figure the css framework out.

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 
 <!--head tag includes data of data (meta data)-->
 <head>
-<title>HamChops - We Got the Meats</title>
+<title>HamShops - We Got the Stuff</title>
 <!--rel is the attribute of the type of file whereas href is the 
 destination of the file (ex .ico or .css)--> 
 	<link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
@@ -18,15 +18,17 @@ destination of the file (ex .ico or .css)-->
 
     <div>
 	
-	<a href="." class="text-2xl no-underline text-grey-darkest hover:text-blue-dark font-sans font-bold ">HamChops</a>
+	<a href="." class="text-2xl no-underline text-grey-darkest hover:text-blue-dark font-sans font-bold ">HamShops</a>
 	
 	</div>
 	
-    <span class="text-xs text-grey-dark">We Got the Meats</span>
+    <span class="text-xs text-grey-dark">We Got the Stuff</span>
 
   </div>
-
-  <div class="sm:mb-0 self-center">
+  
+  
+<!-- NEW SECTION-->
+	<div class="sm:mb-0 self-center">
     <!-- <div class="h-10" style="display: table-cell, vertical-align: middle;"> -->
     <a href="./menu" class="text-md no-underline text-black transition hover:text-blue-500 ml-2 px-1">Menu</a>
     <a href="./about-us" class="text-md no-underline text-grey-darker transition  hover:text-blue-500 ml-2 px-1">About Us</a>
@@ -36,26 +38,47 @@ destination of the file (ex .ico or .css)-->
   </div>
 </nav>
 
-
-  <div class="container mx-auto rounded-t shadow bg-cover bg-bottom m-4" style="background-image: url(https://ak.picdn.net/shutterstock/videos/34449439/thumb/1.jpg?ip=x480)">
- 	
-<div class="mb-16"> </div>
-    
-    <div class="text-center text-white pb-16 mb-16">
-      <h1 class="font-sans font-thin">Reality is just a matter of perception.</h1>
-      <h2 class="font-thin text-sm tracking-wide mt-2 mb-4">There are no limits, except for those that we impose on ourselves. Join the future</h2>
-  
-      	<a href="#" class="rounded bg-white text-black  shadow-md hover:bg-black hover:text-white transition p-4 font-bold inline-block mb-16">
-      		Apply Now
-      	</a>
-      
-    
-    </div>
-  </div>
-  
+	<div class="section bg-image flex overflow-hidden h-screen text-gray-800">
+        <div class="hero bg-gray-200 text-center grid md:grid-cols-2 border w-4/6 m-auto p-8 bg-opacity-90 rounded-lg">
+          <img class="icon m-auto rounded-lg" src="https://4.bp.blogspot.com/-ucPoZt7SKLk/XLfEpEXrxQI/AAAAAAAAJDw/O5DB7a2xDNMxegebwlYay6xVhxt-08iHwCLcBGAs/s1600/Coca%2BCola%2BGlazed%2BBaked%2BSpiral%2Bham.jpg" alt="" />
+          <div class="text m-auto text-lg md:ml-5 p-5 md:text-left">
+            <div class="head text-3xl mb-3 font-semibold">Why We Love HamShops?</div>
+            <div class="desc">We R Goated. Lol</div>
+          </div>
+        </div>
+      </div>
 
 
-
+<!-- NEW SECTION-->
+			<div class="section cards mx-auto grid md:grid-cols-3 md:px-12 bg-red-400 border-b-4 border-t-4  text-gray-800 ">
+        <div class="card text-sm shadow-lg bg-white max-w-sm m-5 hover:bg-gray-100 transition mx-auto  sm:mx-auto md:m-5 overflow-hidden flex flex-col rounded">
+          <div class="img"><img class="w-full h-full" src="https://instagram.fdet1-2.fna.fbcdn.net/v/t51.2885-19/s150x150/95399611_574775113160524_1845823261301014528_n.jpg?_nc_ht=instagram.fdet1-2.fna.fbcdn.net&_nc_ohc=KTgnFOWCSfMAX9_Inc-&tp=1&oh=4eb99950d5fbb4eff90b4349125317c3&oe=6045A245" alt="" /></div>
+          <div class="text p-5 pt-2 text-center">
+            <div class="title font-semibold my-2 text-xl text-red-700">Clown 1</div>
+            <div class="desc">HI</div>
+          </div>
+		  <a href="https://google.com"class="mx-auto p-5 bg-green-300 mb-3 font-bold text-white hover:text-green-300 transition rounded"> Click Here </a>
+        </div>
+        <!-- eachcard 1^ -->
+        <div class="card text-sm shadow-lg bg-white max-w-sm m-5 hover:bg-gray-100 transition mx-auto  sm:mx-auto md:m-5 overflow-hidden flex flex-col rounded">
+          <div class="img"><img class="w-full h-full" src="https://instagram.fdet1-2.fna.fbcdn.net/v/t51.2885-19/s150x150/95399611_574775113160524_1845823261301014528_n.jpg?_nc_ht=instagram.fdet1-2.fna.fbcdn.net&_nc_ohc=KTgnFOWCSfMAX9_Inc-&tp=1&oh=4eb99950d5fbb4eff90b4349125317c3&oe=6045A245" alt="" /></div>
+          <div class="text p-5 pt-2 text-center">
+            <div class="title font-semibold my-2 text-xl text-red-700">Clown 2</div>
+            <div class="desc">HI</div>
+          </div>
+		  <a href="https://google.com"class="mx-auto p-5 bg-green-300 mb-3 font-bold text-white hover:text-green-300 transition rounded"> Click Here </a>
+        </div>
+        <!-- eachcard 2^-->
+        <div class="card text-sm shadow-lg bg-white max-w-sm m-5 hover:bg-gray-100 transition mx-auto  sm:mx-auto md:m-5 overflow-hidden flex flex-col rounded">
+          <div class="img"><img class="w-full h-full" src="https://instagram.fdet1-2.fna.fbcdn.net/v/t51.2885-19/s150x150/95399611_574775113160524_1845823261301014528_n.jpg?_nc_ht=instagram.fdet1-2.fna.fbcdn.net&_nc_ohc=KTgnFOWCSfMAX9_Inc-&tp=1&oh=4eb99950d5fbb4eff90b4349125317c3&oe=6045A245" alt="" /></div>
+          <div class="text p-5 pt-2 text-center">
+            <div class="title font-semibold my-2 text-xl text-red-700">Clown 3</div>
+            <div class="desc">HI</div>
+          </div>
+		  <a href="https://google.com"class="mx-auto p-5 bg-green-300 mb-3 font-bold text-white hover:text-green-300 transition rounded"> Click Here </a>
+        </div>
+        <!-- eachcard 3 ^-->
+      </div>
 
 
 
